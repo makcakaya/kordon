eea02a5b5cec6ee78fbdae20188750d2293ae028
Mert Akcakaya <mert@biltera.com.tr>
2016-12-11 17:53:28 +0300

    Basic feature/test implementation

---
+++ src/Kordon.Tests/CordTests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kordon.Tests
+{
+    public class CordTests
+    {
+        [Fact]
+        public void Build()
+        {
+            var cord = new Cord<string>();
+        }
+
+        [Fact]
+        public void CallingRaiseWithoutRegisterationDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+
+            cord.Raise("Hello World!");
+        }
+
+        [Fact]
+        public void CallingRegisterDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+            var handler = new Action<string>((a) => { });
+
+            cord.Register(handler);
+        }
+
+        [Fact]
+        public void CallingRegisterMultipleTimesDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+            var handler = new Action<string>(a => { });
+
+            cord.Register(handler);
+            cord.Register(handler);
+        }
+
+        [Fact]
+        public void CallingUnregisterWithoutRegistrationDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+            var handler = new Action<string>(a => { });
+
+            cord.Unregister(handler);
+        }
+
+        [Fact]
+        public void CallingUnregisterWithNullDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+
+            cord.Unregister(null);
+        }
+
+        [Fact]
+        public void CallingRegisterWithNullDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+
+            cord.Register(null);
+        }
+
+        [Fact]
+        public void CallingRaiseAfterRegisteringNullHandlerDoesNotThrow()
+        {
+            var cord = new Cord<string>();
+
+            cord.Register(null);
+            cord.Raise("test");
+        }
+
+        [Fact]
+        public void CallingRaiseDoesNotBlock()
+        {
+            var executed = false;
+            var delay = TimeSpan.FromMilliseconds(100);
+            var cord = new Cord<string>();
+            var handler = new Action<string>(a =>
+            {
+                Thread.Sleep(delay);
+                executed = true;
+            });
+            cord.Register(handler);
+
+            cord.Raise("Helo World!");
+            Assert.False(executed);
+        }
+
+        [Fact]
+        public void HandlerIsExecutedSerially()
+        {
+            var arguments = new List<int>();
+            for (var i = 0; i < 100; i++)
+            {
+                arguments.Add(i);
+            }
+
+            var handlerIndex = 0;
+            var match = true;
+            var cord = new Cord<int>();
+
+            var handler = new Action<int>(a =>
+            {
+                if (a != arguments[handlerIndex])
+                {
+                    match = false;
+                }
+
+                handlerIndex++;
+            });
+
+            cord.Register(handler);
+
+            foreach (var arg in arguments)
+            {
+                cord.Raise(arg);
+            }
+
+            Task.Delay(100).Wait();
+
+            Assert.Equal(arguments.Count, handlerIndex);
+            Assert.True(match);
+        }
+
+        [Fact]
+        public void AllHandlersAreExecutedAsync()
+        {
+            var executionCount = 0;
+            var executionSync = new object();
+            var delay = TimeSpan.FromMilliseconds(100);
+            var cord = new Cord<string>();
+            var handler1 = new Action<string>(a =>
+             {
+                 lock (executionSync)
+                 {
+                     executionCount++;
+                 }
+
+                 Task.Delay(delay).Wait();
+             });
+
+            var handler2 = new Action<string>(a =>
+             {
+                 lock (executionSync)
+                 {
+                     executionCount++;
+                 }
+
+                 Task.Delay((int)delay.TotalMilliseconds * 2).Wait();
+             });
+
+            var handler3 = new Action<string>(a =>
+             {
+                 lock (executionSync)
+                 {
+                     executionCount++;
+                 }
+
+                 Task.Delay((int)delay.TotalMilliseconds * 3).Wait();
+             });
+
+            cord.Register(handler1);
+            cord.Register(handler2);
+            cord.Register(handler3);
+
+            cord.Raise("test");
+            Task.Delay((int)delay.TotalMilliseconds * 4).Wait();
+
+            Assert.Equal(3, executionCount);
+        }
+
+        [Fact]
+        public void RaiseMultipleTimesCallsHandlersInOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Kordon/Cord.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kordon
+{
+    public sealed class Cord<T>
+    {
+        private readonly List<T> _executionArgs = new List<T>();
+        private readonly List<Action<T>> _executionHandlers = new List<Action<T>>();
+        private readonly AutoResetEvent _autoReset = new AutoResetEvent(false);
+        private readonly object _syncObject = new object();
+        private readonly Queue<T> _raiseQueue = new Queue<T>();
+        private readonly List<Action<T>> _handlers = new List<Action<T>>();
+
+        public Cord()
+        {
+            Task.Factory.StartNew(Executor, TaskCreationOptions.LongRunning);
+        }
+
+        public void Raise(T arg)
+        {
+            lock (_syncObject)
+            {
+                if (_handlers.Count == 0) { return; }
+
+                _raiseQueue.Enqueue(arg);
+                _autoReset.Set();
+            }
+        }
+
+        public void Register(Action<T> handler)
+        {
+            lock (_syncObject)
+            {
+                if (handler == null) { return; }
+                if (_handlers.Contains(handler)) { return; }
+                else { _handlers.Add(handler); }
+            }
+        }
+
+        public void Unregister(Action<T> handler)
+        {
+            lock (_syncObject)
+            {
+                if (handler == null) { return; }
+                if (_handlers.Contains(handler)) { _handlers.Remove(handler); }
+            }
+        }
+
+        private void Executor()
+        {
+            while (true)
+            {
+                _autoReset.WaitOne();
+                _executionArgs.Clear();
+                _executionHandlers.Clear();
+
+                lock (_syncObject)
+                {
+                    while (_raiseQueue.Count > 0)
+                    {
+                        _executionArgs.Add(_raiseQueue.Dequeue());
+                    }
+
+                    _executionHandlers.AddRange(_handlers);
+                }
+
+                if (_executionArgs.Count == 0 || _executionHandlers.Count == 0) { return; }
+
+                foreach (var handler in _executionHandlers)
+                {
+                    Task.Run(() =>
+                    {
+                        foreach (var arg in _executionArgs)
+                        {
+                            handler(arg);
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
